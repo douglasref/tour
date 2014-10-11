32d7993414c8625a4da4db733358601bde3af270
douglas <douglas@inf06.adm.fea.fumec.local>
2014-10-10 22:40:09 -0300

    Callbacks and tests.

--- public_html/js/tips.js
+++ public_html/js/tips.js
@@ -4,7 +4,8 @@
         position: {x: 0, y: 0},
         select: {type: 'circle', size: 100},
         parent: 'body',
-        event: 'onload',
+        eventIn: 'onload',
+        eventOut: 'click',
         animation: 'fade',
         anyElementInteraction: true,
         spacing: 20,
@@ -18,49 +19,51 @@
         functionAfter: function() {
         }
     }
-    $.fn.tips = function(options) {
-        var settings = $.extend(defaults, options);
-
-        $('body').after('<div id="tip-background"><span id="tip-text"></span></div>');
+    $.fn.tips = function(options, callback) {
+        $(this).append('<span class="tip-text"></span>');
+            var settings = $.extend(defaults, options);
+        if($('#tip-background').length <= 0)
+            $('body').after('<div id="tip-background"></div>');
 
         if (this.css('position') === "static")
             this.css({position: 'relative', top: 0, right: 0, left: 0});
-
+        
         //console.log($(this).offset());
-        var widthOuter = $(this).outerWidth(true);
-        var width = $(this).width();
-        var height = $(this).height();
-        var offsetX = $(this).offset().left;
-        var offsetY = $(this).offset().top;
+        var element = $(this);
+        var bg = $('#tip-background');
+        var widthOuter = $(element).outerWidth(true);
+        var width = $(element).width();
+        var height = $(element).height();
+        var offsetX = $(element).offset().left;
+        var offsetY = $(element).offset().top;
         var spaceRight = $(window).width() - (offsetX + width);
         var spaceLeft = $(window).width() - (width + spaceRight);
         var spaceBottom = $(window).height() - (offsetY + height);
         var spaceTop = $(window).height() - (height + spaceBottom);
 
         this.css({'z-index': 991});
-        var label = $('#tip-text');
-        $(label).html(settings.text);
-        var pos = _position();
-        $(label).css({position: 'absolute', top: pos.top, left: pos.left});
+        var label = $(this).find('.tip-text').html(settings.text);
+        var pos = _position(label);
+        $(label).css({position: 'relative', top: pos.top, left: pos.left});
+        _setEventOut();
         //alert(settings.animation);
         return this;
 
-        function _position() {
-            alert($(label).width());
+        function _position(label) {
             var pos = {top: 0, left: 0};
             if (_showHorizontal()) {
-                pos.top = offsetY + (label.height() / 2) + parseInt(settings.spacing);
+                pos.top = (height / 2) - ((label).height() / 2);
                 if (spaceRight >= spaceLeft) {
-                    pos.left = offsetX + width + parseInt(settings.spacing);
+                    pos.left = width + parseInt(settings.spacing);
                 } else {
-                    pos.left = offsetX - parseInt(settings.spacing);
+                    pos.left = -1 * label.width() - parseInt(settings.spacing);
                 }
             } else {
-                pos.left = offsetX + (width / 2) - (label.width() / 2);
+                pos.left = (width / 2) - ((label).width() / 2);
                 if (spaceTop >= settings.minHeightLabel) {
-                    pos.top = offsetY - (label.height() + settings.spacing);
+                    pos.top =  -1  * settings.spacing - (label.height() / 2); 
                 } else {
-                    pos.top = offsetY + height + settings.spacing;
+                    pos.top = height + settings.spacing / 2;
                 }
             }
             return pos;
@@ -72,6 +75,40 @@
                 return true;
             }
             return false;
+        }
+        function _setEventOut(){
+            switch(settings.eventOut){
+                case 'change':
+                    $(element).change(function(){_destroy();});
+                    break;
+                case 'hover':
+                    $(element).hover(function(){_destroy();}, function(){return false;});
+                    break;
+                case 'keydown':
+                    $(element).keydown(function(){_destroy();});
+                    break;
+                case 'click':
+                default:
+                    $(element).click(function(){_destroy();});
+            }
+        }
+        function _destroy(){
+            var countTips = $('.tip-text').length;
+            var time = 300;
+
+            label.fadeOut(time);
+
+            label.remove();
+            if(jQuery.isFunction(callback)){
+                callback();
+            }
+            countTips = $('.tip-text').length;
+            if (countTips === 0) {
+                bg.fadeOut(time);
+                window.setInterval(function() {
+                    bg.remove();
+                }, time);
+            }
         }
     };
 }(jQuery));
