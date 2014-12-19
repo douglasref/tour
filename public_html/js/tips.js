ffc6feb1a80ca85a7984fccbeb52531b3854ef91
douglas <douglas@inf06.adm.fea.fumec.local>
2014-12-18 22:37:53 -0200

    Define o tamanho do label.

--- public_html/js/tips.js
+++ public_html/js/tips.js
@@ -40,34 +40,45 @@
         var spaceLeft = $(window).width() - (width + spaceRight);
         var spaceBottom = $(window).height() - (offsetY + height);
         var spaceTop = $(window).height() - (height + spaceBottom);
+        var zIndexBg = element.css('z-index');
 
         this.css({'z-index': 991});
         var label = $(this).find('.tip-text').html(settings.text);
         var pos = _position(label);
-        $(label).css({position: 'relative', top: pos.top, left: pos.left});
+        
+        $(label).css({position: 'absolute', top: pos.top, left: pos.left});
         _setEventOut();
         //alert(settings.animation);
         return this;
 
+        // return position of label and set width of label
         function _position(label) {
             var pos = {top: 0, left: 0};
+            var labelWidth = $(window).width() - 2 * parseInt(settings.spacing);
             if (_showHorizontal()) {
                 pos.top = (height / 2) - ((label).height() / 2);
+                labelWidth -= width;
                 if (spaceRight >= spaceLeft) {
                     pos.left = width + parseInt(settings.spacing);
+                    labelWidth -= spaceLeft;
                 } else {
                     pos.left = -1 * label.width() - parseInt(settings.spacing);
+                    labelWidth -= spaceRight;
                 }
             } else {
-                pos.left = (width / 2) - ((label).width() / 2);
+                pos.left = (width / 2) - (label.width() / 2);
+                labelWidth -= pos.left;
                 if (spaceTop >= settings.minHeightLabel) {
                     pos.top =  -1  * settings.spacing - (label.height() / 2); 
                 } else {
                     pos.top = height + settings.spacing / 2;
                 }
             }
+            // defines the label width
+            label.css({width: labelWidth});
             return pos;
         }
+        
         function _showHorizontal() {
             if (spaceRight >= spaceLeft && spaceRight >= $(window).width() * 0.5) {
                 return true;
@@ -100,12 +111,16 @@
 
             label.remove();
             
-            // Verifica se eh hover, pois neste caso precisa de 2 parametro
+            // verifies that is hover, because in this case need 2 parameter
             if(settings.eventOut === 'hover')
                 element.unbind('mouseenter mouseleave');
             else
                 element.unbind(settings.eventOut);
             
+            // retrieves the zindex
+            element.css({'z-index': zIndexBg});
+            
+            // verifies callback
             if(jQuery.isFunction(callback)){
                 //element.fn.tips = null;
                 callback();
